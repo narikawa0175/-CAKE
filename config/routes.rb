4a2548e5fc2397dd07f066bdfb4fd3bdc0c77fd4
EC2 Default User <ec2-user@ip-172-31-30-26.ap-northeast-1.compute.internal>
2022-05-30 09:26:07 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -11,17 +11,26 @@ Rails.application.routes.draw do
   
    root to: 'public/homes#top'
    get '/about' => 'public/homes#about'
+   
    get '/customers/unsubscribe' => 'public/customers#unsubscribe',as:'unsubscribe_customer'
    get '/customers/:id' => 'public/customers#show',as:'customer'
    get '/customers/:id/edit' => 'public/customers#edit',as:'edit_customer'
    patch '/customers/:id' => 'public/customers#update',as:'update_customer'
+   
    post '/addresses' => 'public/addresses#create',as:'create_address'
    get '/addresses/:id' => 'public/addresses#index',as:'addresses'
    get '/addresses/:id/edit' => 'public/addresses#edit',as:'edit_address'
    patch '/addresses/:id' => 'public/addresses#update',as:'address'
    delete '/addresses/:id' => 'public/addresses#destroy',as:'destroy_address'
-   get 'items' => 'public/items#index',as:'items'
-   get 'items/:id' => 'public/items#show',as:'item'
+   
+   get '/items' => 'public/items#index',as:'items'
+   get '/items/:id' => 'public/items#show',as:'item'
+   
+   get '/cart_items' => 'public/cart_items#index',as:'cart_items'
+   post '/cart_items' => 'public/cart_items#create',as:'create_cart_item'
+   delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all',as:'destroy_cart_items'
+   patch '/cart_items/:id' => 'public/cart_items#update',as:'update_cart_item'
+   delete '/cart_items/:id' => 'public/cart_items#destroy',as:'destroy_cart_item'
   
   namespace :admin do
    root to: 'homes#top'
